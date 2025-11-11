9a8ae8bc3112894be9b7f6c950e722100c199145
SFFOUZIA <143606421+SFFOUZIA@users.noreply.github.com>
2025-11-11 21:44:58 +0530

    Add files via upload

---
+++ greeting.py
@@ -0,0 +1,7 @@
+first_name = input("Enter your first name: ")
+Second_name = input("Enter your second name: ")
+
+fullname = first_name + " " + Second_name
+
+print("Hello,", fullname +"! Welcome to the Python program")
+
