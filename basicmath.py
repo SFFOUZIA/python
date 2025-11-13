676ebfb8863d68a3c73deb8cb3162fd936891b1c
SFFOUZIA <143606421+SFFOUZIA@users.noreply.github.com>
2025-11-13 20:44:37 +0530

    Add files via upload

---
+++ basicmath.py
@@ -0,0 +1,13 @@
+a=int(input("Enter the first number:"))
+b=int(input("Enter the second number:"))
+
+sum = a + b
+sub = a - b
+mul = a * b
+div = a / b
+
+print()
+print("Addition:", sum,
+      "\nSubtraction:", sub,
+     "\nMultiplication:",mul,
+     "\nDivision:", div)
