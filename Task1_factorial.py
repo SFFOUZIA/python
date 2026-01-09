af9251e052afc5ecea73afadb0429b8eba471cf4
SFFOUZIA <143606421+SFFOUZIA@users.noreply.github.com>
2026-01-10 02:13:12 +0530

    Add files via upload

---
+++ Task1_factorial.py
@@ -0,0 +1,8 @@
+def factorial(n):
+    result = 1
+    for i in range(1, n + 1):   # Loop from 1 to n
+        result *= i              # Multiply result by i
+    return result
+
+num = int(input("Enter a number: "))
+print(f"Factorial of {num} is:  {factorial(num)}")
---
+++ Task2_math.py
@@ -0,0 +1,9 @@
+import math
+number = float(input("Enter a number: "))
+square_root = math.sqrt(number)
+natural_log = math.log(number)
+sine__value = math.sin(number)
+
+print(f"Square root: {square_root}")
+print(f"logarithm: {natural_log}")
+print(f"Sine: {sine__value}")
